860b1a1e0d4475438ecda3c0df20d8cba9bb9bf4
Eric Burrell <ericburrell231@gmail.com>
2024-02-24 15:55:49 -0600

    API updated to return JSON data instead of array of strings

--- order_input_api.py
+++ order_input_api.py
@@ -4,6 +4,24 @@ import subprocess
 app = Flask(__name__)
 
 
+def parse_output_data(output):
+    total_orders_index = output.find("Total Orders:")
+    if total_orders_index == -1:
+        return jsonify({'success': False, 'error': 'Total Orders not found'}), 500
+
+    order_string = output[total_orders_index:]
+    order_string_array = order_string.split('\n')[1:-1]
+    order_data = {}
+    for order_item_string in order_string_array:
+        last_key_index = order_item_string.find(" .")
+        first_value_index = order_item_string.find(". ") + 2
+        key = order_item_string[2:last_key_index]
+        value = int(order_item_string[(first_value_index):])
+        order_data[key] = value
+
+    return jsonify({'success': True, 'output': order_data}), 200
+
+
 @app.route('/process_orders', methods=['POST'])
 def process_orders():
     data = request.json
@@ -16,12 +34,7 @@ def process_orders():
     stdout, stderr = process.communicate(input=packages_input)
 
     if process.returncode == 0:
-        total_orders_index = stdout.find("Total Orders:")
-        if total_orders_index != -1:
-            order_summary = stdout[total_orders_index:]
-            return jsonify({'success': True, 'output': order_summary.split('\n')[1:-1]}), 200
-        else:
-            return jsonify({'success': False, 'error': 'Total Orders not found'}), 500
+        return parse_output_data(stdout)
     else:
         return jsonify({'success': False, 'error': stderr}), 500
 
