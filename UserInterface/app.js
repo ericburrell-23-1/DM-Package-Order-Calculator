31ef8ceb733a02aced974535b30f1bed9030610e
Eric Burrell <ericburrell231@gmail.com>
2024-02-24 16:40:13 -0600

    UI boilerplate added

---
+++ UserInterface/app.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, React!</h1>
+      <p>This is a basic React app.</p>
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
