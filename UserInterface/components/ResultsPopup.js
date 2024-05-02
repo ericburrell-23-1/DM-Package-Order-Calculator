f38e38d1f5d55ae3d3b6478458a1ca2bcea77135
Eric Burrell <ericburrell231@gmail.com>
2024-05-02 08:19:44 -0500

    Enabled printing feature

--- UserInterface/components/ResultsPopup.js
+++ UserInterface/components/ResultsPopup.js
@@ -1,25 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Popup from "reactjs-popup";
 import "./components.css";
 
 const ResultsPopup = ({ open, setOpen, resultsData }) => {
+  const [printMode, setPrintMode] = useState(false);
   const closeModal = () => setOpen(false);
+  const handlePrint = () => {
+    window.print();
+    setPrintMode(false);
+  };
+
+  useEffect(() => {
+    if (printMode) {
+      handlePrint();
+    }
+  });
+
+  const results = (
+    <div className="modal" id="order-results">
+      <a className="close" onClick={closeModal}>
+        &times;
+      </a>
+      <h1 className="popup-header">Order Totals</h1>
+      <div className="results">
+        {Object.keys(resultsData).map((key) => (
+          <div key={key} className="results-line">
+            <p>{key}</p>
+            <p>{resultsData[key]}</p>
+          </div>
+        ))}
+      </div>
+      <button
+        className="btn-print"
+        type="button"
+        onClick={() => setPrintMode(true)}
+      >
+        <span className="material-symbols-outlined">print</span>
+      </button>
+    </div>
+  );
+
+  if (printMode) {
+    return <div className="print-out">{results}</div>;
+  }
+
   return (
     <Popup open={open} closeOnDocumentClick onClose={closeModal}>
-      <div className="modal">
-        <a className="close" onClick={closeModal}>
-          &times;
-        </a>
-        <h1 className="popup-header">Order Totals</h1>
-        <div className="results">
-          {Object.keys(resultsData).map((key) => (
-            <div key={key} className="results-line">
-              <p>{key}</p>
-              <p>{resultsData[key]}</p>
-            </div>
-          ))}
-        </div>
-      </div>
+      {results}
     </Popup>
   );
 };
